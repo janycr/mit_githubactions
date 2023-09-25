7d7d785e6ecb811db1bbe54ebe81984c9d4ce317
Alejandro Valverde <janycr@gmail.com>
2023-09-25 17:28:00 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app     = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  
+app.listen(3000, function(){
+    console.log('Running on port 3000!')
+})
+
+// export app for testing
+module.exports = app;
