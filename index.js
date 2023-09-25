c51fe1e9e2ddc4bbcc94e3c63e6cd0fc2934b5d8
Alejandro Valverde <janycr@gmail.com>
2023-09-25 17:34:00 -0600

    Includes supertest

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app     = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World')
   })
 
-  
+/*  
 app.listen(3000, function(){
     console.log('Running on port 3000!')
-})
+})*/
 
 // export app for testing
 module.exports = app;
---
+++ index.test.js
@@ -0,0 +1,16 @@
+const supertest = require("supertest");
+const app = require("./index");
+const request = supertest(app);
+it('Hello World',async done => {
+    expect(1).toBe(1);;
+    done();
+});
+
+var server = app.listen(3000,function(){
+    console.log("Running on port 3000");
+});
+
+afterAll(done => {
+    server.close();
+    done();
+});
